23569f4b75deefca287f63cff80722025a6629f7
Rajan Sonkar <rajan.sonkar.ece23@itbhu.ac.in>
2026-01-22 20:42:17 +0530

    hoi ne w chande

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: "https://devscribe-backend-4by2.onrender.com/api",
 });
 
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
