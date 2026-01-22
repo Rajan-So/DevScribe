0bff1d408e3e977c1b0b082870bb9f648f0b238f
Rajan Sonkar <rajan.sonkar.ece23@itbhu.ac.in>
2026-01-22 14:09:39 +0530

    Hardcode API URL for Netlify test

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
-      : "https://devscribe-backend.onrender.com/api",
+  baseURL: "https://devscribe-backend.onrender.com/api",
 });
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 export default api;
